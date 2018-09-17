ead7e150892cbf3884f3f685f9c81fae5da5db8b
Kamil <kwiatostan97@gmail.com>
2018-09-17 18:43:03 +0000

    First Look

--- app.js
+++ app.js
@@ -7,7 +7,19 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.get('/', function(req, res){
-   res.send("This is root page "); 
+   res.render("home"); 
+});
+
+app.get('/drummers', function(req, res){
+    res.render('drummers');
+});
+
+app.get('/guitarists', function(req, res){
+   res.render('guitarists');
+});
+
+app.get("/registration", function(req, res){
+   res.render('Registration');
 });
 
 
